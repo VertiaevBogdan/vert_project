dff94a5ade593ee7481570a447850e4aad56a829
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2024-09-04 23:32:55 +0200

    Vue js local server is available now on http://172.18.0.4:5173/

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,10 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import 'vuetify/_styles.scss'
+import MainHeader from './components/main-page/main-header/MainHeader.vue'
 
 loadFonts()
 
-createApp(App).use(router).use(vuetify).mount('#app')
-
-
+const app = createApp(App).use(router).use(vuetify);
+app.mount('#app');
+app.component('main-header', MainHeader);
