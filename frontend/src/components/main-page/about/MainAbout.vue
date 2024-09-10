7226f437f7a9f8cb430dfd964cbd8e4d52bc62ee
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2024-09-10 18:09:24 +0200

    About section

---
+++ frontend/src/components/main-page/about/MainAbout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <div class="about_content">
+      <h3 class="primary-color bold">About Us</h3>
+      <h1 class="about_main_title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, quaerat.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, aut debitis est explicabo itaque molestias necessitatibus optio quasi quidem sunt.</p>
+      <div>
+        <v-btn
+        class="nav_link--is-signup"
+        text="Sign Up"
+        text-color="white"
+        size="large"
+      />
+      <v-btn
+        class="nav_link"
+        text="Learn More"
+        size="large"
+        variant="outlined"
+      />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/global.scss";
+.nav_link {
+  color: $main-text-color !important;
+  border-color: $primary-color;
+}
+
+.nav_link--is-signup {
+  color: $main-text-color !important;
+}
+</style>
