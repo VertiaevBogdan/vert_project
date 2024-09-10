bfa2255ab8e2d68317b29de3f503535412a0884e
Vertiaev Bogdan <bogdan.vertiaev@olc.cz>
2024-09-10 23:27:18 +0200

    Preview section

--- frontend/src/components/main-page/MainPage.vue
+++ frontend/src/components/main-page/MainPage.vue
@@ -1,17 +1,20 @@
 <template>
   <MainHeader/>
   <MainAbout/>
+  <EducationPreview/>
 </template>
 
 <script>
 import MainHeader from '@/components/main-page/header/MainHeader.vue'
 import MainAbout from '@/components/main-page/about/MainAbout.vue'
+import EducationPreview from '@/components/main-page/education-preview/Preview.vue'
 
 
 export default {
   components: {
     MainHeader,
-    MainAbout
+    MainAbout,
+    EducationPreview,
   },
   setup() {
 
